d8ad9285c4f553fbc7504e50914746d2825fce0b
Bryce Gardner <35595059+meanderingwalrus@users.noreply.github.com>
2019-05-08 10:04:29 +1000

    Ali changes to include PSI calculations

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <math.h>
 // User selectable variables
 #define SAMPLE_PERIOD 25 // time between samples in ms
 #define DISPLAY_PERIOD 200
@@ -12,7 +13,16 @@
 int value_recorded = 0;
 int value_displayed = 0;
 float ema_value = 0;
-
+float Vdd = 5.0; //check if 5 exactly, might be slightly off
+int PSI = 0;
+float voltage = 0;
+float area = 0.001794509139657; //cross sectional area of tube in m^2 
+float pressure_PSI, pressure_Pa, mass_flow;
+float rho = 1.1225; //air density in kg/m^3
+float radius_pipe;
+float radius25 = 5.975;
+float Cd = 0.6; //coeff of discharge of an orfice meter, supposed to be between 0.6-0.85
+//float beta = 
 // Display Settings
 #include <SPI.h>
 #include <Adafruit_GFX.h>
@@ -39,7 +49,7 @@ int values[NUM_GRAPH_VALS] = {};
 int loop_count = 0;
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(READ_PIN,INPUT);
 
   display.begin(SSD1306_SWITCHCAPVCC);
@@ -69,20 +79,29 @@ void loop() {
   if(!value_recorded && (millis() % SAMPLE_PERIOD) < 2){ // these lines service analog write every samplePeriod ms
     int read_value = analogRead(READ_PIN);
 
+    voltage = (read_value*Vdd)/1023;
+    pressure_PSI = (7.5)*(voltage - 2.492669);  //7.5 = 15/2 from the formula. voltage to PSI from a similar pressure sensor, change if not accurate
+    pressure_Pa = pressure_PSI*6894.75729; //in Pascals
+    //mass_flow = sqrt((pressure_Pa*2)*rho)/(1/pow(area,2); //volumeric flow rate in m^3/s
+    //mass_flow = 
     for(int i = NUM_GRAPH_VALS; i >= 1; i --){ // scroll values along the matrix
       values[i] = values[i-1];
     }
 
-    values[0] = read_value;
-
-    ema_value = (1.0-EMA_WEIGHT)*ema_value + EMA_WEIGHT*((float) read_value);
+    //values[0] = read_value;
+    values[0] = pressure_PSI;
+    ema_value = (1.0-EMA_WEIGHT)*ema_value + EMA_WEIGHT*((float) read_value); //averaging?
 
     // print out values, compatiable with Arduino Serial Plotter
+    Serial.print("\n read_value: ");
     Serial.print(read_value);
-    Serial.print(" ");
-    Serial.println(ema_value);
+    //Serial.print(", ema_value: ");
+    //Serial.println(ema_value);
+    Serial.print(", voltage: ");
+    Serial.print(voltage);
+    Serial.print(", PSI: ");
+    Serial.print(pressure_PSI);
     value_recorded = 1;
-
   }
   else if(value_recorded && (millis() % SAMPLE_PERIOD) > 2) {
     value_recorded = 0;
